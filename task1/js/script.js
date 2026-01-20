8677a2d85d621cf364d4d7396f8fd6fa8a5b41f9
Matia Paki <matia.m.paki@gmail.com>
2026-01-19 23:24:53 -0500

    Completed

--- task1/js/script.js
+++ task1/js/script.js
@@ -1,5 +1,253 @@
 "use strict";
 
+// QUESITON 2
+
+function setup(){
+    createCanvas(600,600);
+    background(0);
+
+    drawEllipse(100,100,100,100,255,70,100);
+    drawEllipse(200,200,60,100,200,100,100);
+    drawEllipse(300,300,120,120,150,100,100);
+}
+
+function drawEllipse(x,y,w,h,r,g,b){
+    fill(r,g,b);
+    ellipse(x,y,w,h);
+
+}
+// QUESTION 3
+
+/*
+let rect1 = {
+
+    x: 50,
+    y: 50,
+    width: 50,
+    height: 50,
+
+    fill: {
+
+        r: 255,
+        g: 0,
+        b: 0
+    }
+}
+
+
+let rect2 = {
+
+    x: 300,
+    y: 250,
+    width: 50,
+    height: 50,
+
+    fill: {
+
+        r: 0,
+        g: 255,
+        b: 0
+    }
+}
+
+
+let rect3 = {
+
+    x: 500,
+    y: 400,
+    width: 50,
+    height: 50,
+
+    fill: {
+
+        r: 0,
+        g: 0,
+        b: 255
+    }
+}
+
+function setup() {
+
+    createCanvas(600, 600);
+}
+
+function draw() {
+    background(0);
+    drawRectangles();
+    moveRect3();
+    keyPressed();
+    mouseClicked();
+}
+
+function drawRectangles() {
+
+    fill(rect1.fill.r, rect1.fill.g, rect1.fill.b);
+    rect(rect1.x, rect1.y, rect1.width, rect1.height);
+
+    fill(rect2.fill.r, rect2.fill.g, rect2.fill.b);
+    rect(rect2.x, rect2.y, rect2.width, rect2.height);
+
+    fill(rect3.fill.r, rect3.fill.g, rect3.fill.b);
+    rect(rect3.x, rect3.y, rect3.width, rect3.height);
+}
+
+function moveRect3() {
+
+    rect3.y = rect3.y - 1;
+
+    if (rect3.y > height || rect3.y < 0) {
+
+        rect3.y = height;
+    }
+
+    if (rect2.x > width || rect2.x < 0) {
+        rect2.x = 0;
+    }
+
+    if (rect1.x > width || rect1.x < 0) {
+        rect1.x = 0;
+    }
+}
+
+function keyPressed() {
+
+    if (keyIsDown(32)) {
+        rect2.x = rect2.x + 10;
+    }
+}
+
+function mouseClicked() {
+
+    if (mouseIsPressed) {
+        rect1.x += 10;
+    }
+}
+*/
+
+///////////////////////////////////////////////////////
+
+// QUESTION 4
+
+/*
+const rect1 = {
+
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 600,
+
+    fill: {
+        r: 0,
+        g: 200,
+        b: 225,
+    }
+}
+
+const rect2 = {
+    x: 200,
+    y: 0,
+    width: 200,
+    height: 600,
+
+
+    fill: {
+        r: 0,
+        g: 20,
+        b: 225,
+    }
+}
+
+const rect3 = {
+
+    x: 400,
+    y: 0,
+    width: 200,
+    height: 600,
+
+    fill: {
+        r: 5,
+        g: 20,
+        b: 125,
+    }
+}
+
+
+
+function setup() {
+
+    createCanvas(600, 600);
+}
+
+function draw() {
+
+    background(0);
+    drawRectangles();
+
+    //if (mouseX < 200 && mouseX > 200 && mouseX < 400 && mouseX > 400 && mouseX < 600) {
+        rect1.fill = 255;
+    }
+
+    if (mouseX < 200) {
+        rect1.fill = {
+            r: 255,
+            g: 255,
+            b: 255,
+
+        };
+    } else {
+        rect1.fill = {
+            r: 0,
+            g: 200,
+            b: 225,
+        };
+    }
+
+    if (mouseX < 400 && mouseX > 200) {
+        rect2.fill = {
+            r: 255,
+            g: 255,
+            b: 255,
+
+        };
+    } else {
+        rect2.fill = {
+            r: 0,
+            g: 20,
+            b: 225,
+        };
+    }
+
+
+    if (mouseX < 600 && mouseX > 400) {
+        rect3.fill = {
+            r: 255,
+            g: 255,
+            b: 255,
+
+        };
+    } else {
+        rect3.fill = {
+            r: 5,
+            g: 20,
+            b: 125,
+        };
+    }
+
+function drawRectangles() {
+
+    fill(rect1.fill.r, rect1.fill.g, rect1.fill.b);
+    rect(rect1.x, rect1.y, rect1.width, rect1.height);
+
+    fill(rect2.fill.r, rect2.fill.g, rect2.fill.b);
+    rect(rect2.x, rect2.y, rect2.width, rect2.height);
+
+    fill(rect3.fill.r, rect3.fill.g, rect3.fill.b);
+    rect(rect3.x, rect3.y, rect3.width, rect3.height);
+}
+*/
+
+///////////////////////////////////////////////////////
+
 
 /*
 // QUESTION 5
@@ -110,6 +358,8 @@ function draw(){
 
 // QUESTION 7
 
+/*
+
 let circleSize = 40;
 let circleColor;
 
@@ -152,3 +402,5 @@ function keyPressed(){
 function mousePressed(){
     shapeChange = !shapeChange;
 }
+
+*/
