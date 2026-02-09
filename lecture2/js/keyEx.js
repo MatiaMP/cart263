e7c42e8ec5fe4c9c499bc2a01ff688573cd98137
Matia Paki <matia.m.paki@gmail.com>
2026-02-09 10:06:41 -0500

    lecture2

---
+++ lecture2/js/keyEx.js
@@ -0,0 +1,20 @@
+
+window.onload = function(){
+    console.log("keys");
+
+    let speedX = 5;
+    let boxA = this.document.querySelector("#boxA");
+
+    window.addEventListener("keydown", 
+        function(e) {
+    if(e.key === "ArrowRight"){
+        console.log(parseInt(boxA.style.left))
+        let currentPos = parseInt(boxA.style.left)
+        boxA.style.left = currentPos+speedX+"px"
+    }
+})
+
+
+}
+
+    
